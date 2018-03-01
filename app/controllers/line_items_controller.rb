a3fa55e72fbeb38937b9a084fd8a83d4ce9b9fea
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2018-03-01 10:12:14 -0500

    use ERB template to create JS that will execute on client using jQ

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,6 +32,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_url}
+        format.js
         format.json { render :show,
           status: :created, location: @line_item }
       else
@@ -48,7 +49,6 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.update(line_item_params)
         format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
-        format.js
         format.json { render :show, status: :ok, location: @line_item }
       else
         format.html { render :edit }
